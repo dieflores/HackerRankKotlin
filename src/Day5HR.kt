0b3889d299873db7611db45e2252d8f291e7b6d8
dieflores <60197427+dieflores@users.noreply.github.com>
2020-12-16 10:36:53 -0300

    commit de los dias 2,3,4,5 de HRank usando Kotlin

---
+++ src/Day5HR.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+object Day5HackerRank {
+    private val scanner: Scanner = Scanner(System.`in`) //Desde aqui
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val n: Int = scanner.nextInt()
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?") // hasta aqui ya estaba en el ejercicio.
+
+        for (i in 1..10) {
+            println(n.toString() + " x " + i + " = " + n * i) // solo agregué el ciclo for
+        }
+    }
+}
